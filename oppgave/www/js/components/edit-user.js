1159f94235e4df44b2902a08257d57ff12b5a935
henrikgranlund <61779794+henrikgranlund@users.noreply.github.com>
2020-12-17 11:19:55 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,53 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  //Renders a HTML site with input fields that gets automatically filled, and can be edited
+ render() {
+    return html`
+      <head>
+        <title>Edit a user</title>
+      </head>
+      <body>
+        <h1 style="text-align: center;"> Edit a user </h1>
+        <form onsubmit="javascript: return false;" id="userForm" method="POST">
+            <div style="text-align: center;">
+                <label for="email">Email</label>
+                <input type="email" id="uname" name="uname" value="${user.uname}">
+                <br />
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" value="${user.firstName}">
+                <br />
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" value="${user.lastName}">
+                <br />
+                <label for="oldpwd">Old Password</label>
+                <input type="password" id="oldpwd" name="oldpwd" type="text">
+                <br />
+                <label for="newpwd">New Password</label>
+                <input type="password" id="pwd" name="pwd" type="text">
+                <br /><br />
+                <input type="submit" @click=${this.updateUser} id="submitForm" name="editUser" value="Update"></input>
+            </div>
+        </form>
+      </body>
+    `;
+  }
+
+  updateUser(e) {
+    const dataForm = new FormData(e.target.form);
+    console.log(e)
+    fetch('api/updateUser.php', {
+     method: 'POST',
+     body: dataForm
+    }).then(res=>res.json())
+      .then(data=>{
+        if (data.status=='success') {
+            console.log("User got updated!");
+        } else {
+            console.log("Oops! Something went wrong. Please try again.");
+        }
+      })
+  }
 
 }
 customElements.define('edit-user', EditUser);
